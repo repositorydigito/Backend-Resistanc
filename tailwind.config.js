e3dfddfb6fea2f58096786a84a3996163e023cee
aizencode <aizencode@gmail.com>
2025-11-26 14:28:15 -0500

    laptop empresa: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
