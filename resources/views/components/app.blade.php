38b51c2b40a4c4d93d45b21ca6a45c04f48ad08e
aizencode <aizencode@gmail.com>
2025-11-26 12:43:52 -0500

    laptop empresa: html

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -48,14 +48,14 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     {{-- Fin aos scroll --}}
 
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    {{-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
         <style>
         </style>
-    @endif
+    @endif --}}
 
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('css')
 
